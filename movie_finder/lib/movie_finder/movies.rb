7c99e624951e9e13d4f25ebe0d8bc2b709d21a0c
Nicholas Halloran <nmhalloran@hotmail.com>
2017-10-13 22:40:21 +0000

    Partially functioning object instantiator in MovieFinder::Movies.

--- movie_finder/lib/movie_finder/movies.rb
+++ movie_finder/lib/movie_finder/movies.rb
@@ -1,3 +1,19 @@
 class MovieFinder::Movies
+  attr_accessor :title, :starring, :type, :rating, :summary
+
+  def self.make_movie_info
+    movies = MovieFinder::Scraper.theater_scraper
+
+    movies.css("div.show").each_with_index do |movie, i|
+
+      movie_1 = self.new
+      movie_1.title = movie.css('h2').text
+      movie_1.starring = movie.css('h3 + p').text.split(", ")[0] + " and " + movie.css('h3 + p').text.split(", ")[1]
+      movie_1.type = movie.css("h2 + p").text.split(" ")[0]
+      movie_1.rating = movie.css("h2 + p").text.split(" ")[2]
+      movie_1.summary = movie.css("p.cat + p").text
+      binding.pry
+    end
+  end
 
 end
