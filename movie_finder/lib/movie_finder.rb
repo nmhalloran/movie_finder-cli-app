2016c7180b6eddf464d04538ab36d8bb87707fdd
Nicholas Halloran <nmhalloran@hotmail.com>
2017-10-13 03:00:08 +0000

    Fandango pushing back against scraping. Looking into this tomorrow.

--- movie_finder/lib/movie_finder.rb
+++ movie_finder/lib/movie_finder.rb
@@ -1,5 +1,6 @@
 require "nokogiri"
 require "pry"
+require "open-uri"
 
 require "movie_finder/version"
 
--- movie_finder/lib/movie_finder/cli.rb
+++ movie_finder/lib/movie_finder/cli.rb
@@ -1,5 +1,9 @@
 class MovieFinder::CLI
-
-  puts "Hello movie lover!"
+  attr_accessor :zip_code
+  @zip_code = 02132
+  def call
+    MovieFinder::Scraper.zip_code_scraper(@zip_code)
+    puts "Hello movie lover!"
+  end
 
 end
